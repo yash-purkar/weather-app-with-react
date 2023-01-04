48b1d35e487f7ad9a8ddc1b3776b793240a36081
yashpurkar <yashpurkar7079@gmail.com>
2023-01-04 13:59:22 +0530

    got data

--- src/Component/Temp.js
+++ src/Component/Temp.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import cloudy from './Icons/cloudy.svg'
+import React, { useEffect } from 'react';
+import cloudy from './Icons/cloudy.svg';
 
 const Temp = () => {
+  const [inputCity, setInputCity] = React.useState("Ahmednagar");
+
+
+  const getWeather = async () => {
+    try {
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=2d9f020388f73c365b0cc6457b04233e`);
+      let data = await response.json();
+      // console.log(data);
+      const { temp, humidity } = data.main;
+      const name = data.name;
+      const { sunrise, sunset } = data.sys;
+      console.log(temp, humidity, name, sunrise, sunset);
+
+
+
+    }
+    catch (err) {
+      console.log("Not found");
+    }
+  }
+
+  useEffect(() => {
+    getWeather();
+  }, [])
 
 
   return (
     <>
       <div className="main-div">
         <div className="search">
-          <input type="text" placeholder="Enter City Name" autoFocus="off" className='input-city' />
-          <button className='search-btn'>Search</button>
+          <input type="text"
+            value={inputCity}
+            onChange={(e) => setInputCity(e.target.value)}
+            placeholder="Enter City Name" autoFocus="off" className='input-city' />
+          <button onClick={getWeather} className='search-btn'>Search</button>
         </div>
       </div>
 
-      <articles className="widget">
+      <article className="widget">
         <div className="icon">
           <img src={cloudy} alt="" />
         </div>
@@ -32,7 +59,7 @@ const Temp = () => {
         <div className="date">
           {new Date().toDateString()}
         </div>
-      </articles>
+      </article>
     </>
   )
 }
