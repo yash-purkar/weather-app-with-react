995a86542559e6cbfc530aa2ffdcc488ecca5a31
yashpurkar <yashpurkar7079@gmail.com>
2023-01-05 15:43:24 +0530

    added loading

--- src/Component/Temp.js
+++ src/Component/Temp.js
@@ -6,12 +6,14 @@ const Temp = () => {
   const [inputCity, setInputCity] = useState("Ahmednagar");
   const [weatherData, setWeatherData] = useState({});
   const [notFound, setNotFound] = useState(false);
+  const [loading, setLoading] = useState(true);
 
 
   const getWeather = async () => {
     try {
       let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity.replace(" ", "")}&appid=2d9f020388f73c365b0cc6457b04233e`);
       let data = await response.json();
+      setLoading(false)
       // console.log(data);
       const { temp, humidity } = data.main;
       const { main } = data.weather[0];
@@ -46,24 +48,29 @@ const Temp = () => {
 
   return (
     <>
-
-      <div className="parent">
-        <h1>Weather App</h1>
-        <div className="main-div">
-          <div className="search">
-            <input type="text"
-              value={inputCity}
-              onChange={(e) => setInputCity(e.target.value)}
-              placeholder="Enter City Name" autoFocus="off" className='input-city' />
-            <button onClick={getWeather} className='search-btn'>Search</button>
+      {
+        loading ? <h2 id='loading'>Loading...</h2>
+          :
+          <div className="parent">
+            <h1>Weather App</h1>
+            <div className="main-div">
+              <div className="search">
+                <input type="text"
+                  value={inputCity}
+                  onChange={(e) => setInputCity(e.target.value)}
+                  placeholder="Enter City Name" autoFocus="off" className='input-city' />
+                <button onClick={getWeather} className='search-btn'>Search</button>
+              </div>
+            </div>
+            {
+              notFound ? <h2 id='not-found'>City Not Found🤪!!</h2>
+                :
+                <TempCard weatherInfo={weatherData} />
+            }
           </div>
-        </div>
-        {
-          notFound ? <h1>City Not Found🤪!!</h1>
-            :
-            <TempCard weatherInfo={weatherData} />
-        }
-      </div>
+      }
+
+
     </>
   )
 }
